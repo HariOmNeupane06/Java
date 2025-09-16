daa522635ec4aea1f782304002ddb19ea29cb275
Hari Om Neupane <hariom.neupane06@gmail.com>
2025-09-16 22:10:52 +0545

    loop

--- Day2/LoopsBasics.java
+++ Day2/LoopsBasics.java
@@ -1,7 +1,12 @@
 public class LoopsBasics {
     public static void main(String[] args) {
-        if(true){
-            System.out.println("hello world");
+        // if(true){
+        //     System.out.println("hello world");
+        // }
+
+        for(int i=0; i<=10; i++){
+            System.out.println(i);
         }
+        
     }
 }
