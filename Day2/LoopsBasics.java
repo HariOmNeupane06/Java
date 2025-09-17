23e5ebeb4b97ecb7693b3f58143ee208afba4699
Hari Om Neupane <hariom.neupane06@gmail.com>
2025-09-17 22:49:56 +0545

    Print even or odd number

--- Day2/LoopsBasics.java
+++ Day2/LoopsBasics.java
@@ -1,12 +1,30 @@
+import java.util.Scanner;
 public class LoopsBasics {
     public static void main(String[] args) {
         // if(true){
         //     System.out.println("hello world");
         // }
 
-        for(int i=0; i<=10; i++){
-            System.out.println(i);
-        }
+        // for(int i=0; i<=10; i++){
+        //     System.out.println(i);
+        // 
         
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if(num % 2 == 0) {
+            System.out.println(num + " is Even.");
+        } else {
+            System.out.println(num + " is Odd.");
+        }
+
+        sc.close();
+    
+
+
+
     }
 }
