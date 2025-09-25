a13c1e7230ef74458e929b7a7c2afc2eb424cc33
Hari Om Neupane <hariom.neupane06@gmail.com>
2025-09-25 21:32:07 +0545

    commit

--- doWhile.java
+++ doWhile.java
@@ -2,7 +2,7 @@ public class doWhile {
     public static void main(String[] args) {
         int num = 1;
         while(num <= 8) {
-            System.out.println("Number: " + i);
+            System.out.println("the number is : " + i);
             i++;  // increment to avoid infinite loop
         }
     }
