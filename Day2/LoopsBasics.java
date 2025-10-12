fce747649156bc5cc3ddf6a8e5804eed309e14ff
Hari Om Neupane <hariom.neupane06@gmail.com>
2025-10-12 20:37:40 +0545

    commits

--- Day2/LoopsBasics.java
+++ Day2/LoopsBasics.java
@@ -21,7 +21,7 @@ public class LoopsBasics {
         if(num % 2 == 0) {
             System.out.println(num + " is a Even.");
         } else {
-            System.out.println(num + " is a Odd.");
+            System.out.print(num + " is a Odd.");
         }
 
         sc.close();
