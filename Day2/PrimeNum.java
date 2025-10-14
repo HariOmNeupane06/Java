4c987872509b68e6461695f4f7a597b7e7a5a5f0
Hari Om Neupane <hariom.neupane06@gmail.com>
2025-10-14 10:12:55 +0545

    commit

---
+++ Day2/PrimeNum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class PrimeNum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the number up to which you want prime number: ");
+        int n = in.nextInt();
+
+        System.out.println("Prime numbers up to " + n + " are:");
+
+        for (int i = 2; i <= n; i++) { // Loop through each number from 2 to n
+            boolean isPrime = true; // Assume the number is prime
+
+            // Check divisibility from 2 to sqrt(i)
+            for (int j = 2; j <= Math.sqrt(i); j++) {
+                if (i % j == 0) { // If divisible by any number
+                    isPrime = false; // Not prime
+                    break; // Stop checking
+                }
+            }
+
+            if (isPrime) { // If prime, print it
+                System.out.print(i + " ");
+            }
+        }
+
+        in.close();
+    }
+}
