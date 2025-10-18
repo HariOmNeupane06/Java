944f8dec36e6d59f8f25715e63284535bf85080e
Hari Om Neupane <hariom.neupane06@gmail.com>
2025-10-18 07:57:53 +0545

    commit

--- Day2/temperature.java
+++ Day2/temperature.java
@@ -6,7 +6,6 @@ public class temperature {
          float tempC = sc.nextFloat();
          float tempF = (tempC * 9/5) + 32 ;
 
-     System.out.println(" The tempe
-     rature of " + tempC +" Celsius  " + tempF + " frehenheit");
+     System.out.println(" The temperature of " + tempC +" Celsius is " + tempF + " frehenheit");
     }
 }
