e139b2ecb3be0983a64f49276fe6fcd7f8176d6d
Hari Om Neupane <hariom.neupane06@gmail.com>
2025-10-20 20:43:31 +0545

    commit

---
+++ Day1/Input.java
@@ -0,0 +1,18 @@
+// package Day1;
+
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+
+        // System.out.println(sc.nextLine());      // It takes whole line like    hey i am sidd. 
+        // System.out.println("Enter the roll no: ");
+        // System.out.println(sc.next());          //  It takes only first single word.
+        int rollno = sc.nextInt();
+        System.out.println("Your roll no is: "+ rollno);
+        
+        
+
+    }
+}
---
+++ Day2/if_Else.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class if_Else {
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter your salary: ");
+    int salary = sc.nextInt();
+    int bonus;
+    
+    if (salary >=25000 ){
+        bonus =2000 ; 
+    }else{
+        bonus = 1000;
+    }
+    int total = salary + bonus;
+  System.out.println("the total salary with bonus is " + total);
+}
+}
+// if our salary is greater than 25000 then add bonus 2000,otherwise add bonus 1000.
