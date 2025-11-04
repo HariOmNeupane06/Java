98b4f1c70809df2814f49b4343e864a4f0e7b1ba
Hari Om Neupane <hariom.neupane06@gmail.com>
2025-11-04 21:20:44 +0545

    commit

--- Day2/CaseCheck.java
+++ Day2/CaseCheck.java
@@ -6,8 +6,12 @@ char ch = sc.next().trim().charAt(1);
 
 // System.out.println(sc.next().trim());
 
-System.out.println(ch);
-
+// System.out.println(ch);
+if (ch >= 'a'&& ch<= 'z'){
+    System.out.println("Lowercase");
+}else{
+    System.out.println("UpperCase");
+}
 
 
    } 
