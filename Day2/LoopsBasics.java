889596ed95fc4a2c02e2184e784cbea08fdad507
Hari Om Neupane <hariom.neupane06@gmail.com>
2025-11-09 22:49:56 +0545

    commit

--- Day2/LoopsBasics.java
+++ Day2/LoopsBasics.java
@@ -16,7 +16,7 @@ public class LoopsBasics {
         // for(int i=0; i<=10; i++){
         //     System.out.println(i);
     
-        
+        // userInput
         Scanner in = new Scanner(System.in);
 
         System.out.print("Enter  number: ");
