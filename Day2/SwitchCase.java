8f20bd55086d9e11a672c32499f6eb1a67367783
Hari Om Neupane <hariom.neupane06@gmail.com>
2025-11-11 21:12:33 +0545

    commit

--- Day2/SwitchCase.java
+++ Day2/SwitchCase.java
@@ -3,30 +3,66 @@ public class SwitchCase {
    public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    String fruit = sc.next();
+    // String fruit = sc.next();
 
-    switch(fruit){
+    // switch(fruit){
 
-        case "Mango":
-        System.out.println(" best fruit");
-        break;
+    //     case "Mango":
+    //     System.out.println(" best fruit");
+    //     break;
 
-        case "banana":
-        System.out.println(" a large fruit");
-        break;
+    //     case "banana":
+    //     System.out.println(" a large fruit");
+    //     break;
 
-        case "watermelon":
-        System.out.println(" watery fruit");
-        break;
+    //     case "watermelon":
+    //     System.out.println(" watery fruit");
+    //     break;
 
-        case "grapes":
-        System.out.println(" black ball fruit");
-        break;
+    //     case "grapes":
+    //     System.out.println(" black ball fruit");
+    //     break;
 
+    //     default:
+    //     System.out.println("Sorry!! Please enter a valid fruit name");
+    // }
+
+
+    int Emp_Id  =  sc.nextInt();
+    String department = sc.next();
+     
+    switch (Emp_Id) {
+
+        case 1:
+            System.out.println("Sidd Neupane");
+            break;
+
+        case 2:
+            System.out.println("Bishal Neupane");
+            break;
+
+        case 3:
+        System.out.println("Employee no 3.");
+        switch(department){
+
+            case "IT":  
+            
+            System.out.println("IT Department");
+            break;
+
+            case "Management":
+            System.out.println("Management Department");
+            break;
+
+            default:
+            System.out.println("No department entered");
+
+        }            
+         break;
         default:
-        System.out.println("Sorry!! Please enter a valid fruit name");
+        System.out.println("Enter the correct Employee ID");
+        break;
     }
-
 
 
    } 
