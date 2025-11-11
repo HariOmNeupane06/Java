b74e29fb59782d77317f35a78d0399958c12b354
Hari Om Neupane <hariom.neupane06@gmail.com>
2025-11-11 23:24:11 +0545

    commit

---
+++ Day3/Functions.java
@@ -0,0 +1,28 @@
+// package Day3;
+
+import java.util.Scanner;
+public class Functions {
+    public static void main(String[] args) {
+
+        sum();
+    }
+
+      static void sum(){
+
+
+          Scanner sc = new Scanner(System.in);
+
+         System.out.println("Enter the number 1: ");
+          int num1 =  sc.nextInt();
+
+         System.out.println("Enter the number 2: ");
+         int num2 = sc.nextInt();
+         
+         int sum = num1 + num2;
+         System.out.println("the sum of  num1 & num2 is: "+ sum); 
+       }
+
+// access  modeifiers  (we'll look in oop )
+
+    }
+
