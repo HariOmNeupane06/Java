4113d1e72c88099e6191af5a9a7f18a0c24eea3d
Hari Om Neupane <hariom.neupane06@gmail.com>
2025-11-12 22:12:31 +0545

    commit

---
+++ Day3/part1.java
@@ -0,0 +1,24 @@
+// public class part1 {
+//  static void greet(){
+//     System.out.println("Hello");
+//  }
+//     public static void main(String[] args) {
+//         greet();
+//     }
+// }
+
+
+public  class part1{
+
+    static int sum(int a,  int b){
+        int sum = a+b;
+        return sum;
+    }
+
+
+    
+    public static void main(String[] args) {
+        int result = sum(3,5);
+        System.out.println("the sum is :- " + result);
+    }
+}
