3f0f5edcc4927abbe6d162544ce7d6ca9da8e8bd
Hari Om Neupane <hariom.neupane06@gmail.com>
2025-12-10 22:47:04 +0545

    commit

--- Day3/Functions.java
+++ Day3/Functions.java
@@ -12,10 +12,10 @@ public class Functions {
 
           Scanner sc = new Scanner(System.in);
 
-         System.out.println("Enter the number 1: ");
+         System.out.println("Enter  number 1: ");
           int num1 =  sc.nextInt();
 
-         System.out.println("Enter the number 2: ");
+         System.out.println("Enter  number 2: ");
          int num2 = sc.nextInt();
          
          int multiply = num1 * num2;
