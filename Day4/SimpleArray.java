42f2172f84fc861a6b7e144b379c05a585968f96
Hari Om Neupane <hariom.neupane06@gmail.com>
2026-01-03 22:24:46 +0545

    commit

---
+++ Day4/SimpleArray.java
@@ -0,0 +1,11 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        // Create an array of 3 numbers
+        int[] numbers = {1, 2, 3};
+
+        // Print each number
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
