6917b1a2ad7ad9dac387fa9d252c96121a152613
Hari Om Neupane <hariom.neupane06@gmail.com>
2026-01-10 23:09:54 +0545

    commit

--- Day4/SimpleArray.java
+++ Day4/SimpleArray.java
@@ -11,13 +11,37 @@
 // }
 
 
+// public class SimpleArray {
+//     public static void main(String[] args) {
+
+//         int[] arr = {5, 10, 15, 20, 25};
+
+//         for (int i = 0; i < arr.length; i++) {
+//             System.out.println(arr[i]);
+//         }
+//     }
+// }
+
+
 public class SimpleArray {
     public static void main(String[] args) {
+        int[] numbers = {12, 45, 7, 23, 89};
 
-        int[] arr = {5, 10, 15, 20, 25};
+        int sum = 0;
+        int max = numbers[0];
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        for (int num : numbers) {
+            sum += num;               // sum of all elements
+            if (num > max) {
+                max = num;            // find maximum number
+            }
         }
+
+        double average = (double) sum / numbers.length;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + average);
+        System.out.println("Maximum: " + max);
     }
 }
+
