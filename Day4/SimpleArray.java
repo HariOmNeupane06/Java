82ccc5008b50d35cf37fa4b2ab7fb568cf71f375
Hari Om Neupane <hariom.neupane06@gmail.com>
2026-01-19 21:49:17 +0545

    commit

--- Day4/SimpleArray.java
+++ Day4/SimpleArray.java
@@ -59,7 +59,7 @@
 public class SimpleArray {
     public static void main(String[] args) {
 
-        int[] a = {1, 2, 3, 4};
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8};
 
         for (int i = a.length - 1; i >= 0; i--) {
             System.out.println(a[i]);
