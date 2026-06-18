5e90c42d3cbf6e5f9efb76c0cfdc3b97d1e5580f
Hari Om Neupane <hariom.neupane06@gmail.com>
2026-06-18 21:58:42 +0545

    ArrayListProblem

--- Day4/ArrayListDemo.java
+++ Day4/ArrayListDemo.java
@@ -1,6 +1,8 @@
+
 import java.util.ArrayList;
 
 public class ArrayListDemo {
+
     public static void main(String[] args) {
 
         // Create an ArrayList
@@ -39,23 +41,89 @@ public class ArrayListDemo {
 // Adds the colors "Red", "Blue", and "Green".
 // Prints all the colors using a loop.
 // Prints the total number of colors using size().
+        ArrayList<String> colors = new ArrayList<>();
+        colors.add("Red ");
+        colors.add("Blue ");
+        colors.add("Green ");
+
+        for (String result : colors) {
+            System.out.println(result);
+        }
+
+        System.out.println("The total numbers of colors are: " + colors.size());
+
+        System.out.println("=========================================================");
+
+//              Q.no 2
+//  Creates an ArrayList<Integer> with:   10, 25, 8, 50, 15, 20
+// Prints all numbers.
+// Finds the largest number.
+// Calculates the sum of all numbers.
+// Counts how many numbers are even.
+// Checks if 50 exists in the list.
+// Removes 8 from the list.
+// Prints the updated list.
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(10);
+        numbers.add(25);
+        numbers.add(8);
+        numbers.add(50);
+        numbers.add(15);
+        numbers.add(20);
+
+// Print all Numbers
+        for (int printAllNumbers : numbers) {
+            System.out.print(printAllNumbers + "  ");
+        }
+// finding Largest Number
+
+        int largest = numbers.get(0);
+
+        for (int num : numbers) {
+            if (num > largest) {
+                largest = num;
+            }
+        }
+        System.out.println(" ");
+        System.out.println("the largers number is: " + largest);
+
+// calculate all numbers.
+        int sum = 0;
+        for (int addNumber : numbers) {
+            sum = sum + addNumber;
+        }
+        System.out.println("The sum of all number:- " + sum);
+
+// count even number
+        int evenCount = 0;
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                evenCount++;
+            }
+
+        }
+        System.out.println("there are " + evenCount + " even numbers.");
+
+// print if 50 exists 
+      if(numbers.contains(50)){
+        System.out.println("\n yes! 50 is present");
+      }else{
+        System.out.println("\n 50 is not present");
+      }
+    
+
+        numbers.remove(Integer.valueOf(8));
+        System.out.println(numbers);
 
 
-ArrayList<String>colors =  new ArrayList<>();
- colors.add("Red ");
- colors.add("Blue ");
- colors.add("Green ");
+//prints the update list
+System.out.println("\nThe Updated List");
+ for(int num: numbers){
+    System.out.print(num  + "  ");
+ }
 
 
-for(String result: colors){
-    System.out.println(result);
-}
 
-
-System.out.println("The total numbers of colors are: "+ colors.size());
-
-
-System.out.println("=========================================================");
 
 
 
