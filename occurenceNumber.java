bd4442ebf07ec074d8811b688687439ee3ae22a4
Hari Om Neupane <hariom.neupane06@gmail.com>
2026-06-20 21:50:37 +0545

    DSA questions solving

--- occurenceNumber.java
+++ occurenceNumber.java
@@ -6,19 +6,22 @@ public class occurenceNumber {
     public static int countOccurrences(int[] arr, int target){
         int count = 0;
         for(int i = 0; i< arr.length; i++){
-            
+            if(arr[i] == 2){
+                count++;
+            }
         }
-
+        return count;
     }
 
 
 
 
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 2, 4, 2, 5};
+        int[] nums = {1, 2, 3, 2, 4, 2, 5,6,2, 2};
         int target = 2;
 
-    int output = countOccurrences(nums, target)
+    int output = countOccurrences(nums, target);
+    System.out.println("The occurence number is :- "+ output);
     }
 }
 
