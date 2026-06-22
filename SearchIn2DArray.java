61bd56e41dea18abfa2f3870a9783c26e583c5f1
Hari Om Neupane <hariom.neupane06@gmail.com>
2026-06-22 18:45:00 +0545

    DSA questions solving

--- SearchIn2DArray.java
+++ SearchIn2DArray.java
@@ -1,8 +1,6 @@
 
-
-import java.util.Arrays;
-
 public class SearchIn2DArray {
+
     public static void main(String[] args) {
         int[][] arr = {
             {23, 4, 1},
@@ -10,23 +8,24 @@ public class SearchIn2DArray {
             {78, 99, 34, 56},
             {18, 12}
         };
-        
-        int target = 56;
-        int[] ans = search(arr, target); // format of return value {row, col}
-        System.out.println(Arrays.toString(ans));
-    }
+        int maxOutput = max(arr);
+        System.out.println("    ");
+        System.out.println("the maximum number in this matrix is :- " + (maxOutput));
 
-    static int[] search(int[][] arr, int target) {
-        // Loop through each row
+    }
+    // find max  
+
+    static int max(int[][] arr) {
+        int max = Integer.MIN_VALUE;
         for (int row = 0; row < arr.length; row++) {
-            // Loop through each column in the current row
             for (int col = 0; col < arr[row].length; col++) {
-                if (arr[row][col] == target) {
-                    return new int[]{row, col};
+                if (arr[row][col] > max) {
+                    max = arr[row][col];
+
                 }
             }
         }
-        // Return {-1, -1} if the target is not found in the array
-        return new int[]{-1, -1};
+        return max;
     }
+
 }
