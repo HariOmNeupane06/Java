c4beedf4673b3758442d2540b990b3fc9c9d0b05
Hari Om Neupane <hariom.neupane06@gmail.com>
2026-06-23 22:36:08 +0545

    DSA questions solving

---
+++ Day5/DSA Questions/BinarySearch.java
@@ -0,0 +1,44 @@
+// public class BinarySearch {
+
+//     public static void main(String[] args) {
+//        int []arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45} ;
+//        int target = -12;
+
+//        int  result = binarySearchMethod( arr, target);
+//        if(result != -1){
+//     System.out.println("Element found at index :- " + result);
+//        }else{
+//         System.out.println("Element not found");
+//        }
+
+
+//     }
+
+//     static int binarySearchMethod( int []arr , int target ){
+//         int start = 0;
+//         int end = arr.length -1 ;
+
+//         while (start <= end){
+//             int mid = start + (end - start)/2;
+            
+
+
+//             if(arr [mid] == target){
+//                 return mid ;  //  element found;
+            
+
+
+//             }else if(arr[mid]>target){
+//                 end = mid -1;
+//             }else{
+//                 start = mid + 1;
+//             }
+//         }
+        
+        
+//         return -1;
+//     }
+//         }
+
+
+
