24a91a846fa46719bc890a95386720246a89b42c
Hari Om Neupane <hariom.neupane06@gmail.com>
2026-07-07 17:43:39 +0545

    Remove compiled .class files and add .gitignore

--- Day8/Recursion/CountBS.java
+++ Day8/Recursion/CountBS.java
@@ -1,32 +1,31 @@
 public class CountBS {
     public static void main(String[] args) {
-         // counting the Binary Search using recursive call
+        // Binary Search using recursive call
 
-         int []arr = {10, 20, 30, 40, 50, 60, 70};
-         int target = 80;
+        int[] arr = {10, 20, 30, 40, 50, 60, 70};
+        int target = 80;
+
+        int ans = search(arr, target, 0, arr.length - 1);
+
+        System.out.println(ans);
     }
-    static int search(int []arr, int target, int s , int e  ){
 
-        int count = 0;
+    static int search(int[] arr, int target, int s, int e) {
 
-        if(s > e ){
+        if (s > e) {
             return -1;
         }
 
-        int mid = s+(e-s)/2;
+        int mid = s + (e - s) / 2;
 
-        if(arr[mid] == target){
+        if (arr[mid] == target) {
             return mid;
         }
 
-        if(target < arr[mid]){
-            return search(arr,target , s, mid-1);
+        if (target < arr[mid]) {
+            return search(arr, target, s, mid - 1);
         }
 
-        return
-
+        return search(arr, target, mid + 1, e);
     }
-
-
-
 }
--- Day8/Recursion/PowerCalc.java
+++ Day8/Recursion/PowerCalc.java
@@ -1,26 +1,25 @@
+
 public class PowerCalc {
 
-    public static int  CalculatePower(int x, int n ){
-if(n==0){
-return 1;
-}
-if(x == 0){
-    return 0;
-}
+    public static int CalculatePower(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (x == 0) {
+            return 0;
+        }
         // work
-        int xPownm1 = CalculatePower(x, n-1);
+        int xPownm1 = CalculatePower(x, n - 1);
         int xPowern = x * xPownm1;
         return xPowern;
 
-
-
     }
-    public static void main(String[] args) {
-        
-int x=2,  n = 5;
-int ans =  CalculatePower(x , n);
-System.out.println(ans);
 
+    public static void main(String[] args) {
+
+        int x = 2, n = 5;
+        int ans = CalculatePower(x, n);
+        System.out.println(ans);
 
     }
 }
