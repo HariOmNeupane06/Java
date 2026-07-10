576d68228cbc3f98ddfb0d3ed511d9759a11d788
Hari Om Neupane <hariom.neupane06@gmail.com>
2026-07-10 10:47:51 +0545

    Constructor Overloading

---
+++ ConstructorOverloading.java
@@ -0,0 +1,20 @@
+/* 
+class ClassName {
+
+    // Constructor 1
+    ClassName() {
+        // code
+    }
+
+    // Constructor 2
+    ClassName(dataType parameter) {
+        // code
+    }
+
+    // Constructor 3
+    ClassName(dataType parameter1, dataType parameter2) {
+        // code
+    }
+}
+
+*/
