b7d24b879660f4595d0cbd417555576636f5bec2
Hari Om Neupane <hariom.neupane06@gmail.com>
2026-09-04 22:38:26 +0545

    Encapsulation

---
+++ Day9/00P Part 2/tempCodeRunnerFile.java
@@ -0,0 +1,16 @@
+class Student{
+    private String name;
+
+    public void setName(String Name){
+this.name = Name;
+
+    }
+    public String GetName(){
+        return name;
+    }
+    public static void main(String[] args) {
+        Student s =  new Student();
+        s.setName("Hari");
+        System.out.println(s.GetName());
+    }
+}
